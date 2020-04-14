282aae2a8b30ff6fd31877394435ffa30342cbf0
Bryce Hinds <bryce.hinds@onshoreoutsourcing.com>
2020-04-14 16:43:35 -0400

    Completed module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -41,7 +41,7 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             var type = parts[2].ToLower();
-            var isWeighted = parts[3];
+            var isWeighted = Convert.ToBoolean(parts[3]);
             BaseGradeBook gradeBook;
             if (type == "standard")
                 gradeBook = new StandardGradeBook(name, isWeighted);
