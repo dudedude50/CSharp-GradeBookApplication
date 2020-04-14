9f001bf70fa5021f4d195ee49bd2eeb368ef4f98
Bryce Hinds <bryce.hinds@onshoreoutsourcing.com>
2020-04-14 13:17:34 -0400

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,19 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (averageGrade >= 90)
+                return 'A';
+            else if (averageGrade >= 80)
+                return 'B';
+            else if (averageGrade >= 70)
+                return 'C';
+            else if (averageGrade >= 60)
+                return 'D';
+            else
+                return 'F';
+        }
     }
 }
